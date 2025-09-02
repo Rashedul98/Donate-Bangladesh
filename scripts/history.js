ec6dfd02849ee189b4f2e5d7e18e272e9ad75537
Rashedul98 <thisisrashedul@gmail.com>
2025-09-02 22:06:23 +0600

    Date and Time zone added to history section

--- scripts/history.js
+++ scripts/history.js
@@ -1,9 +1,15 @@
 function showHistory(amount, tagName) {
+
+    // hide h1 element
+    // document.getElementById('history-h1').classList.add('hidden');
+
     const div = document.createElement('div');
     const HistoryTag = document.getElementById(tagName).innerText;
+    const Time = new Date();
     div.innerHTML = `
-        <div class= "border border-gray-400 p-6 mt-8 rounded-lg">
-            <h1 class= "text-base font-semibold"> ${amount} Tk is Donated for ${HistoryTag}</h1><br>
+        <div class= "border border-gray-400 p-6 mt-8 rounded-lg flex flex-col justify-center">
+            <h1 class= "text-base font-semibold"> ${amount} Tk is Donated for ${HistoryTag}</h1>
+            <h1>Date: ${Time}</h1>
         </div>
     `
     document.getElementById('History-page').appendChild(div)
