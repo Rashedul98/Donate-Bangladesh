56e84a9af88fcecff6f68bfac3cccce45e906fd8
Rashedul98 <thisisrashedul@gmail.com>
2025-09-02 12:47:30 +0600

    added donation fields and interactive buttonds

--- scripts/amountInput.js
+++ scripts/amountInput.js
@@ -3,21 +3,41 @@ function donationAmountInput(id, specific) {
     // take input amount and parse it
     const amountValue = document.getElementById(id).value;
     const parsedAmount = parseFloat(amountValue);
+    console.log(parsedAmount);
 
-    // take donated amount
-    const showDonatedAmount = document.getElementById('show-donated-amount').innerText;
-    const ParsedShowDonatedAmount = parseFloat(showDonatedAmount);
+    if (isNaN(parsedAmount)) {
 
-    // total amount
-    const total = parsedAmount + ParsedShowDonatedAmount;
+        alert("please add numbers onyl");
+        return;
 
-    // add specific donation
-    specificAmount(specific, parsedAmount);
+    } else {
 
 
-    // show donated amount
-    document.getElementById('show-donated-amount').innerText = parsedAmount + ParsedShowDonatedAmount;
+        // take donated amount
+        const showDonatedAmount = document.getElementById('show-donated-amount').innerText;
+        const ParsedShowDonatedAmount = parseFloat(showDonatedAmount);
 
+        // add specific donation
+        const defaultSpecificAmount = document.getElementById(specific).innerText;
+        // parsing into float
+        const parsedDefaultSpecificAmount = parseFloat(defaultSpecificAmount);
+        // adding into specific donation
+        document.getElementById(specific).innerText = parsedDefaultSpecificAmount + parsedAmount;
+
+
+        // total amount
+        const total = parsedAmount + ParsedShowDonatedAmount;
+
+        // Add updated donated amount and remove "Donate Now text"
+        document.getElementById('show-donated-amount').classList.remove('hidden');
+        document.getElementById("BDT-btn").classList.remove('hidden');
+        document.getElementById('donte-now-text').classList.add('hidden');
+
+        document.getElementById('show-donated-amount').innerText = total;
+
+    }
+
+    // Clear input fields
     document.getElementById(id).value = '';
 
 
