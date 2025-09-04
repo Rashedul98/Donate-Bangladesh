9f8e0cdb1eaec7c9af11b9f3cb1c7b0cd03ca6fd
Rashedul98 <thisisrashedul@gmail.com>
2025-09-05 00:38:08 +0600

    History Button hover effect added

--- scripts/buttonEeffects.js
+++ scripts/buttonEeffects.js
@@ -16,4 +16,22 @@ document.getElementById('Donation-btn').addEventListener('mouseout', function (e
 
 })
 
+// History Button - hover effects
+document.getElementById('History-btn').addEventListener('mouseover', function (event) {
+    event.stopImmediatePropagation();
 
+    document.getElementById('Donation-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('Donation-btn').classList.remove('button-effect-hover');
+
+
+    this.classList.add('bg-[#B4F461]');
+    this.classList.add('button-effect-hover');
+
+})
+
+document.getElementById('Donation-btn').addEventListener('mouseout', function (event) {
+
+    document.getElementById('History-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('History-btn').classList.remove('button-effect-hover');
+
+})
